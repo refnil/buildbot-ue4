554274bd467fdf7b489ea8a9aa0df171ab2a7e26
Martin Lavoie <broemartino@gmail.com>
2015-01-04 20:24:42 -0500

    update config

--- config.cmd
+++ config.cmd
@@ -4,3 +4,7 @@ set easy_install=C:\Python27\scripts\easy_install
 set buildbot=C:\Python27\scripts\buildbot
 set slave=C:\Python27\scripts\buildslave
 set install=C:\Python27\python setup.py install
+
+set ip=172.16.24.130
+set port=9989
+set webport=8010
